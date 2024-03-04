306f5dd607fd0a0c780a15d33713db5e4ea9ff49
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-03-04 11:17:02 -0800

    added event listeners and score functionality to the webpage

--- assets/index.js
+++ assets/index.js
@@ -53,6 +53,10 @@ function showQuestion(){
         button.innerHTML= answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
 
     });
 }
@@ -63,6 +67,52 @@ function resetState(){
         answerButtons.removeChild(answerButtons.firstChild)
     }
 }
+
+function selectAnswer(event){
+    var selectedBtn = event.target;
+    var isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+}
+
+function showScore(){
+    resetState();
+    questionElem.innerHTML = "You scored " + score + " out of " + questions.length + "!";
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display= "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () =>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+
+})
 startQuiz();
 
 
